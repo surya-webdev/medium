492d8c6cf06a5cac8ab790671b24f9be2bfd961e
surya-webdev <surya.webdev23@gmail.com>
2024-09-20 11:26:00 +0530

    feat: added blogs routes

--- backend/src/routes/blog.ts
+++ backend/src/routes/blog.ts
@@ -1,4 +1,7 @@
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
 import { Hono } from "hono";
+
 import { verify } from "hono/jwt";
 
 export const blogRoute = new Hono<{
@@ -8,22 +11,160 @@ export const blogRoute = new Hono<{
   };
 }>();
 
+// blogRoute.use("*", (c, next) => {
+//   const prisma: any = new PrismaClient({
+//     datasourceUrl: c.env.DATABASE_URL,
+//   }).$extends(withAccelerate());
+
+//   c.set("prisma", prisma);
+
+//   next();
+// });
+
 blogRoute.use("/*", async (c, next) => {
   // get the jwt from the headers, analyse it return the id
   const header = c.req.header("authorization") || "";
 
   const token = header || "";
 
-  const user = await verify(token, c.env.JWT_SECERET);
+  const user = (await verify(token, c.env.JWT_SECERET)) || "";
+  /////
   console.log(user);
-  next();
+
+  if (user.id) {
+    c.set("jwtPayload", user);
+    await next();
+  } else {
+    c.status(403);
+    return c.json({
+      message: "Your'e not logged in :(((",
+    });
+  }
 });
 
-blogRoute.get("/", (c) => {
-  return c.json({ message: "Hello Hono!" });
+blogRoute.post("/post", async (c) => {
+  //
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const id = c.get("jwtPayload");
+
+  const body = await c.req.json();
+
+  if (id && (body.title || body.content)) {
+    //
+    try {
+      const blog = await prisma.post.create({
+        data: {
+          title: body.title,
+          content: body.content || "",
+          authorId: id?.id,
+        },
+      });
+
+      return c.json({ blog });
+
+      //
+    } catch (error) {
+      console.error(error);
+      c.status(411);
+      return c.json({ message: "ERROR WHILE FETCHING" });
+    }
+    //
+  } else {
+    c.status(300);
+    return c.text("PLEASE GIVE A VALID BLOG");
+  }
+  //
 });
 
-blogRoute.get("/api/v1/blog/:blog", (c) => {
-  const id = c.req.param("blog");
-  return c.text(id);
+//
+
+blogRoute.put("/:id/edit", async (c) => {
+  //
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const body = await c.req.json();
+
+  const authorId = c.get("jwtPayload");
+
+  console.log(authorId);
+
+  const id = c.req.param("id");
+
+  console.log(id);
+
+  if (body.title || body.content) {
+    try {
+      const user = await prisma.post.update({
+        where: {
+          id,
+          authorId: authorId.id,
+        },
+        data: {
+          title: body.title,
+          content: body.content,
+        },
+      });
+
+      return c.json({ user });
+    } catch (error) {
+      console.error(error);
+      return c.json({ error: "FAILDED WHILE FETCHING!" });
+    }
+  } else {
+    c.status(300);
+    return c.json({
+      message: "invalid inputs",
+    });
+  }
+});
+
+// TODO ADDING PAGINATION:
+
+blogRoute.post("/bulk", async (c) => {
+  try {
+    const prisma = new PrismaClient({
+      datasourceUrl: c.env.DATABASE_URL,
+    }).$extends(withAccelerate());
+    const blogs = await prisma.post.findMany();
+    console.log(blogs);
+    if (blogs) {
+      return c.json({ blogs });
+    } else {
+      throw new Error();
+    }
+  } catch (error) {
+    console.error(error);
+    return c.json({ error: "FAILDED WHILE FETCHING!" });
+  }
+});
+
+// id
+
+blogRoute.get("/:id", async (c) => {
+  // 4943b6af-f9d9-463b-a4b2-8482c5b5006c
+
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const id = c.req.param("id");
+
+  if (id) {
+    const blogs = await prisma.post.findUnique({
+      where: {
+        id,
+      },
+    });
+    return c.json({ blogs });
+  } else {
+    c.status(411);
+    return c.json({
+      message: "No blogs by the user",
+    });
+  }
 });
